c68e30ebc28a46869b95014588980c403adca051
Nuria Molina <nuriamolina@MacBook-Pro-de-Nuria.local>
2017-10-18 12:06:05 +0200

    more json info

---
+++ json/testimonios.js
@@ -0,0 +1,38 @@
+//json de los testimonials
+
+var testimonials={
+    "testimonial":[
+        {  
+            "nombre":"Nuria",
+            "foto":"/img/testimonials/6-caminos-para-volverte-una-persona-menos-toxica.jpg",
+            "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/caracteristicas-timidez.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/exitosa1.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/images.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/los-10-rasgos-que-indican-que-eres-una-persona-mas-inteligente-que-los-demas.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/primera_persona_2017_carlos_zanon_nt.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/senales-persona-sociable-4.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        },{  
+            "nombre":"",
+            "foto":"/img/testimonials/szdfyguhijokpñ.jpg",
+            "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
+        }
+        ]};
