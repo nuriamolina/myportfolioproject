c0530fbdda938069d6a20c28dfdfe48fb18cdedd
Nuria Molina <nuriamolina@MBP-de-Nuria.home>
2017-10-30 21:18:26 +0100

    upload json

--- json/testimonios.js
+++ json/testimonios.js
@@ -3,35 +3,35 @@
 var testimonials={
     "testimonial":[
         {  
-            "nombre":"Nuria",
+            "nombre":"Marta",
             "foto":"/img/testimonials/6-caminos-para-volverte-una-persona-menos-toxica.jpg",
             "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Cristina",
             "foto":"/img/testimonials/caracteristicas-timidez.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Alba",
             "foto":"/img/testimonials/exitosa1.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Andrea",
             "foto":"/img/testimonials/images.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Sergio",
             "foto":"/img/testimonials/los-10-rasgos-que-indican-que-eres-una-persona-mas-inteligente-que-los-demas.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Carlos",
             "foto":"/img/testimonials/primera_persona_2017_carlos_zanon_nt.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Edgar",
             "foto":"/img/testimonials/senales-persona-sociable-4.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         },{  
-            "nombre":"",
+            "nombre":"Alberto",
             "foto":"/img/testimonials/szdfyguhijokpñ.jpg",
             "texto":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus. Semper risus in hendrerit gravida rutrum quisque non. A lacus vestibulum sed arcu. Vitae sapien pellentesque habitant morbi tristique senectus. Mattis molestie a iaculis at erat pellentesque adipiscing."
         }
