942358efa2ffb2a01530b34f95ecae459bbab078
Nuria Molina <nuriamolina@MBP-de-Nuria.home>
2017-11-01 20:22:41 +0100

    json y nuevas wireframes

---
+++ json/articulosblog.js
@@ -0,0 +1,44 @@
+//thounght of the day
+
+var articulosblog={
+    "blog":[
+        {
+            "titulo":"Campaña de la Declaración de la Renta 2016",
+            "imagen":"/img/blogimg/1.jpg",
+            "fecha":"13.04.2017",
+            "texto":"Hola!!Espero que estos días de descanso los hayas podido disfrutar porque ya han acabado las vacaciones de semana santa y eso significa volver al trabajo. En esta Newsletter de Fincas Horta os queremos contar que es lo que esta pasando a nuestro alrededor. Para empezar deciros que tenemos una nueva página web. En ella podréis encontrar información en todas esas áreas que Fincas Horta os ofrece. Aquí os dejo en enlace www.fincashorta.es. Queremos empezar contándoos que ya llega la época de la DECLARACIÓN DE LA RENTA. Ya podéis pasaros por nuestro oficina con todos esos papeles y documentos que nosotros nos encargamos.", 
+        },{  
+            "titulo":"La subida del precio de la vivienda y nuestros con nuevos inmuebles",
+            "imagen":"/img/blogimg/2.jpg",
+            "fecha":"08.05.2017",
+            "texto":"Hola!! En esta Newsletter os vamos a contar una noticia que os puede interesar sobre la subida del precio de la vivienda. El precio de la vivienda subiço un 4,7% en 2016, su mayor repunte desde 2007. El precio de la vivienda libre subió una media del 4,7% en 2016 respecto al año anterior, su tercer repunte anual consecutivo tras seis años de caídas y el más elevado desde el ejercicio 2007, según el Índice de Precios de Vivienda (IPV) del Instituto Nacional de Estadística (INE) publicado este miércoles. Entre 2008 y 2013, el precio de la vivienda libre registró descensos anuales y no fue hasta 2014 cuando positivas, con un repunte anual medio del 0,3%, que en 2015 se aceleró hasta el 3,6% y ahora en 2016 hasta el 4,7%. Por tipo de vivienda, el precio de la de segunda mano aumentó un 4,4% en 2016, registrando su mayor crecimiento desde el ejercicio 2007. En el caso de las viviendas nuevas, su precio avanzó una media del 6,5% en 2016, también su mayor repunte desde 2007. Si quereis ver como sigue esta totica podeis encontrarla en aquí.", 
+        },{  
+            "titulo":"Noves lleis per la plusvàlua",
+            "imagen":"/img/blogimg/3.jpg",
+            "fecha":"29.05.2017",
+            "texto":"El Tribunal Constitucional declara nuls determinats articles de la Llei d’hisendes locals que afecten a l'impost de plusvàlua quan es produeix una minusvàlua.El passat dia 11 de maig de 2017 el Ple del Tribunal Constitucional va dictar sentència en relació a una qüestió d'inconstitucionalitat promoguda pel Jutjat Contenciós-Administratiu n. de Jerez de la Frontera en relació amb els articles 107.1, 107.2 a) i 110.4 de la Llei Reguladora de les Hisendes Locals, d'aplicació estatal, relatiu a l'Impost sobre l'increment del valor dels terrenys de naturalesa urbana, conegut popularment com Plusvàlua Municipal. La situació fàctica a què fa referència l'esmentat recurs d'inconstitucionalitat es manifesta en els supòsits en què ha existit una transmissió d'un immoble per qualsevol títol i es produeix en realitat una minusvàlua o pèrdua patrimonial clara. Amb anterioritat, concretament el passat mes de febrer i març, l'Alt Tribunal s'havia pronunciat en el mateix sentit però declarant inconstitucionals els articles de la normativa reguladora d'aquest impost però que afectaven únicament a Guipúscoa i Àlaba. Ara, la decisió de la sentència declara nuls determinats articles de la regulació de l'impost de la Llei d'Hisendes Locals d'aplicació estatal, ja que se sotmeten a tributació en el mateix situacions inexpressives de la capacitat econòmica, impedint que els subjectes passius puguin acreditar aquesta circumstància. La sentència  fa, entre d’altres, la següent reflexió: la circumstància que el naixement de l'obligació tributària es fes dependre, llavors i també ara, de la transmissió d'un terreny, podria ser una condició necessària en la configuració del tribut, però, de cap manera, pot erigir-se en una condició suficient en un tribut l'objecte del qual és l'increment de valor d'un terreny. Al fet d'aquesta transmissió s'ha d'afegir, per tant la necessària materialització d'un increment de valor del terreny, exponent d'una capacitat econòmica real o, si més no, potencial. No obstant això, quan no s'ha produït aquest increment en el valor del terreny transmès, la capacitat econòmica pretesament gravada deixa de ser potencial per convertir-se en irreal o fictícia, violant-se amb això el principi de capacitat econòmica. Així doncs, determina la sentència que el legislador haurà de modificar la Llei Reguladora de les Hisendes Locals relativa a l'Impost sobre l'increment del valor dels terrenys de naturalesa urbana, podent reclamar la devolució en concepte d'ingressos indeguts dels quatre últims anys en els supòsits en que sigui d'aplicació aquesta sentència. Mentrestant, si es produeix el supòsit recollit en aquesta sentència i fins que no s'aclareixi la modificació d'aquests articles de la Llei d'Hisendes Locals, cautelarment entenem que si estem davant del sistema d’autoliquidació, s’hauria de presentar, fer-ne el pagament i després demanar-ne la nul·litat i la conseqüent devolució de l'import ingressat, i si la liquidació la gira l’Ajuntament al subjecte passiu, caldrà  formular-ne recurs.", 
+        },{  
+            "titulo":"Llegó el verano",
+            "imagen":"/img/blogimg/4.jpg",
+            "fecha":"07.07.2017",
+            "texto":"Hola a todos! Como ya sabreis estamos en verano y en Fincas Hortacambiamos nuestro horario laboral. Durante estos meses de verano nuestro horario es: Lunes a Viernes de 9:00 horas - 14:00 horas Lunes de 16:00 horas - 19:00 horas. Esto solo sera así este mes de Julio ya que en Agosto estara cerrado. Pero no os preocupeis, en septiembre volvemos con mucha energia y ocn nuestro horario de siempre. No dudeis en contactar con nosotros por telefono o por correo electronico si teneris cualquier consulta.", 
+        },{  
+            "titulo":"Barcelona és la segona ciutat de l’Estat amb el sòl més car",
+            "imagen":"/img/blogimg/5.jpg",
+            "fecha":"24.10.2017",
+            "texto":"Les compres de sòl urbà s’han reactivat clarament els últims mesos i, per tant, els preus van a l’alça. Segons les dades publicades pel ministeri de Foment, el preu del metre quadrat de sòl urbà a Barcelona s’ha situat el segon trimestre en 491,5 euros, xifra que suposa un augment del 7 % respecte del trimestre anterior i de l’1,4 % respecte al mateix trimestre de l’any passat. Amb aquest preu Barcelona és, de les grans ciutats espanyoles -de més de 50.000 habitants-, la segona més cara, només superada per Granada, on el metre quadrat se situa en 510 euros. Que el mercat del sòl s’ha reactivat ho demostren les dades agregades que publica el ministeri. Durant el segon trimestre, a tot Espanya hi va haver quasi 6.000 transaccions de sòl urbà, amb un augment del 35,2 % respecte del segon trimestre de l’any passat, i la superfície que ha canviat de mans supera els 7,2 milions de metres quadrats, amb un increment del 26,6 %. El valor total d’aquestes compres de sòl puja a 1.074 milions d’euros, un 43 % superior al valor de les compres del segon trimestre de l’any passat. El preu a Espanya del metre quadrat de sòl és de 166,4 euros de mitjana, un 1,8 % superior al mateix trimestre de l’any passat. Així, el preu a la ciutat de Barcelona triplica el preu de mitjana a l’Estat. No obstant, el valor del sòl és lluny dels màxims. Abans de la crisi el preu del metre quadrat a la capital catalana superava els 1.000 euros i fins i tot en plena crisi es va disparar a un màxim de 1.372,7 euros el metre quadrat el quart trimestre del 2009. Però des d’aquell moment els preus van entrar en caiguda lliure fins a un mínim de 327,9 euros el metre quadrat el quart trimestre del 2012. En el conjunt de Catalunya el preu del metre quadrat de sòl urbà el segon trimestre d’enguany se situa en 442,7 euros, amb un augment del 3,9 % respecte del trimestre anterior, i d’un 1,3 % respecte de fa un any.", 
+        }
+    ]
+};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
